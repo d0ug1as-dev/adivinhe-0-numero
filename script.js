f8e9d72509c6bdcf188fcbca95b21c1146262ad1
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2024-07-13 20:18:23 -0300

    script getDom

---
+++ script.js
@@ -0,0 +1,12 @@
+const menssagem = document.getElementById('menssagem')
+
+const inputNum = document.getElementById('inputNum')
+/* Label onde aparecera menssagem de numero baixo ou alto */
+const labelInputNum = document.getElementById('labelInputNum')
+
+const btnEnviar = document.querySelector('.btnEnviar')
+
+/* Todas as li´s onde ficaram as jogadas */
+const tentativa = document.querySelectorAll('.tentativa')
+
+
